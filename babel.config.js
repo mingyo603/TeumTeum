43bcf44091215a3730190294c8f7a3bcc1bdfa49
mingyo603 <20251028@sungshin.ac.kr>
2025-08-14 17:28:58 +0900

    경로 오류 수정

---
+++ babel.config.js
@@ -0,0 +1,24 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './',
+            '@/components': './components',
+            '@/screens': './screens',
+            '@/storage': './storage',
+            '@/utils': './utils',
+            '@/context': './context'
+          },
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+        }
+      ],
+      'react-native-reanimated/plugin'
+    ]
+  };
+};
